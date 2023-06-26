911efa07de25b2e28e220e1854dd5e7d285bd7c8
Christian <cmanalo.io@outlook.com>
2023-06-26 00:06:57 -0700

    update note with strike-through feature

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,11 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Note(props) {
+    const [isDone, setIsDone] = useState(false);
+
+    const strikeThrough = { textDecoration: "line-through" };
+
+    const handleClick = () => {
+        setIsDone(!isDone);
+    };
 
     return (
         <div className="note">
-            <h1> {props.title} </h1>
-            <p> {props.content} </p>
+            <h1>{props.title}</h1>
+            <p
+                style={isDone ? strikeThrough : null}
+                onClick={handleClick}
+            >
+                {props.content}
+            </p>
         </div>
     );
 }
