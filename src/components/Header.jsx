67e20cb91684201eb5c7333d178ef16f816dd67f
Christian <cmanalo.io@outlook.com>
2023-06-27 03:55:43 -0700

    update header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,23 @@
-import React from 'react';
-
+import React, { useState } from 'react';
 
 function Header() {
-    return <header>
-        <h1>MKeeper</h1>
-    </header>
-}
+  const [isBlack, setIsBlack] = useState(false);
 
+  const handleClick = () => {
+    setIsBlack(!isBlack);
+  };
+
+  const headerStyle = {
+    color: isBlack ? 'black' : 'white',
+  };
+
+  return (
+    <header>
+      <h1 style={headerStyle} onClick={handleClick}>
+        Mkeeper
+      </h1>
+    </header>
+  );
+}
 
 export default Header;
