07680f509de4871211ca1235f31da3639bfd382f
Christian <cmanalo.io@outlook.com>
2023-06-29 03:05:05 -0700

    add different prompts

--- src/notes.js
+++ src/notes.js
@@ -1,28 +1,63 @@
 const notes = [
-    {
-      key: 1,
-      title: "Delegation",
-      content:
-        "Q. How many programmers does it take to change a light bulb? A. None – It’s a hardware problem"
-    },
-    {
-      key: 2,
-      title: "Loops",
-      content:
-        "How to keep a programmer in the shower forever. Show him the shampoo bottle instructions: Lather. Rinse. Repeat."
-    },
-    {
-      key: 3,
-      title: "Arrays",
-      content:
-        "Q. Why did the programmer quit his job? A. Because he didn't get arrays."
-    },
-    {
-      key: 4,
-      title: "Hardware vs. Software",
-      content:
-        "What's the difference between hardware and software? You can hit your hardware with a hammer, but you can only curse at your software."
-    }
-  ];
-  
+  {
+    key: 1,
+    title: "Delegation",
+    content:
+      "Q. How many programmers does it take to change a light bulb? A. None – It’s a hardware problem"
+  },
+  {
+    key: 2,
+    title: "Loops",
+    content:
+      "How to keep a programmer in the shower forever. Show him the shampoo bottle instructions: Lather. Rinse. Repeat."
+  },
+  {
+    key: 3,
+    title: "Arrays",
+    content:
+      "Q. Why did the programmer quit his job? A. Because he didn't get arrays."
+  },
+  {
+    key: 4,
+    title: "Hardware vs. Software",
+    content:
+      "What's the difference between hardware and software? You can hit your hardware with a hammer, but you can only curse at your software."
+  },
+  {
+    key: 5,
+    title: "Debugging",
+    content:
+      "Q. Why do programmers prefer dark mode? A. Because light attracts bugs."
+  },
+  {
+    key: 6,
+    title: "Functions",
+    content:
+      "Why did the JavaScript function go to therapy? Because it had too many callbacks."
+  },
+  {
+    key: 7,
+    title: "Memory Management",
+    content:
+      "Q. Why did the programmer go broke? A. Because he lost his cache."
+  },
+  {
+    key: 8,
+    title: "Syntax Error",
+    content:
+      "Why did the programmer always carry a ladder? In case he encountered a high-level language."
+  },
+  {
+    key: 9,
+    title: "Recursion",
+    content:
+      "To understand recursion, you must first understand recursion."
+  },
+  {
+    key: 10,
+    title: "Database",
+    content:
+      "Q. Why did the SQL developer go broke? A. Because he lost his table."
+  }
+];
   export default notes;
